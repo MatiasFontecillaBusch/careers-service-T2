dad5e219d5e526350a63cbb5d18a504beb6c5c4e
MatiasFontecillaBusch <matias.f.fontecilla@gmail.com>
2024-10-31 20:27:01 -0300

    feat: add crud for careers

--- app.ts
+++ app.ts
@@ -1,11 +1,23 @@
 import { Server } from "@grpc/grpc-js";
 import { loadProto } from "@/utils/loadProto";
-import { createCareer } from "@/services/careersService";
+import {
+  createCareer,
+  deleteCareer,
+  getAllCareers,
+  getCareer,
+  updateCareer,
+} from "@/services/careersService";
 import { CareersService } from "@/types";
 
 const careersProto = loadProto("careers") as CareersService;
 
 const server = new Server();
-server.addService(careersProto.Careers.service, { createCareer });
+server.addService(careersProto.Careers.service, {
+  createCareer,
+  getCareer,
+  updateCareer,
+  deleteCareer,
+  getAllCareers,
+});
 
 export default server;
--- src/services/careersService.ts
+++ src/services/careersService.ts
@@ -19,3 +19,73 @@ export const createCareer = catchAsync(
     return callback(null, career);
   }
 );
+
+export const getCareer = catchAsync(
+  async (
+    call: ServerUnaryCall<{ id: string }, unknown>,
+    callback: sendUnaryData<Career>
+  ) => {
+    const { id } = call.request;
+    const career = await Careers.findById(id);
+
+    if (!career) {
+      throw new AppError("Carrera no encontrada", status.NOT_FOUND);
+    }
+    return callback(null, career);
+  }
+);
+
+export const updateCareer = catchAsync(
+  async (
+    call: ServerUnaryCall<{ id: string; name: string }, unknown>,
+    callback: sendUnaryData<Career>
+  ) => {
+    const { id, name } = call.request;
+    if (!name) {
+      throw new AppError(
+        "El nombre de la carrera es necesario para actualizarla",
+        status.INVALID_ARGUMENT
+      );
+    }
+
+    const career = await Careers.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true, runValidators: true }
+    );
+
+    if (!career) {
+      throw new AppError("Carrera no encontrada", status.NOT_FOUND);
+    }
+    return callback(null, career);
+  }
+);
+
+export const deleteCareer = catchAsync(
+  async (
+    call: ServerUnaryCall<{ id: string }, unknown>,
+    callback: sendUnaryData<{ success: boolean }>
+  ) => {
+    const { id } = call.request;
+    const career = await Careers.findByIdAndUpdate(
+      id,
+      { deletedAt: new Date() },
+      { new: true, runValidators: true }
+    );
+
+    if (!career) {
+      throw new AppError("Carrera no encontrada", status.NOT_FOUND);
+    }
+    return callback(null, { success: true });
+  }
+);
+
+export const getAllCareers = catchAsync(
+  async (
+    call: ServerUnaryCall<{}, unknown>,
+    callback: sendUnaryData<{ careers: Career[] }>
+  ) => {
+    const careers = await Careers.find();
+    return callback(null, { careers });
+  }
+);
