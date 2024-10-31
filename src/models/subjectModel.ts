eb4fcc3f192de2aae0fd7565903bfcafb5523f9b
MatiasFontecillaBusch <matias.f.fontecilla@gmail.com>
2024-10-31 20:54:19 -0300

    fix: add credits to subjects

--- src/models/subjectModel.ts
+++ src/models/subjectModel.ts
@@ -4,6 +4,7 @@ const SubjectSchema = new Schema({
   code: { type: String, required: true },
   name: { type: String, maxlength: 250, required: true },
   department: { type: String, maxlength: 250, required: true },
+  credits: { type: Number, required: true, min: 1 },
   semester: { type: Number, required: true, min: 1 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
